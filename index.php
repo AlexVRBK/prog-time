eb08b79ce9afff23cf22df8fecd91c0ac766f8da
AlexVRBK <vvvorobok@gmail.com>
2021-05-14 19:22:38 +0300

    Test Commit!

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
-$testCommit = 1;
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Это заголовок тайтл</title>
+</head>
+<body>
+<p>
+    Это абзац.
+</p>
+
+<p>
+    Это еще один абзац.
+</p>
+
+<p>
+    И еще один абзац.
+</p>
+</body>
+</html>
